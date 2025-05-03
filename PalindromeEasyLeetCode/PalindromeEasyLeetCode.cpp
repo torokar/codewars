62b15ad935553e72ea84137047d7c9355046bc9c
Andrey <andrey.onkhonov95@bk.ru>
2025-05-04 02:04:15 +0800

    чтобы не потерять задачу #https://leetcode.com/problems/palindrome-number/

---
+++ PalindromeEasyLeetCode/PalindromeEasyLeetCode.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //не потерять 
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    return 0;
+}
