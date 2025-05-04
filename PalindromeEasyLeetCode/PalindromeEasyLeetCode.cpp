b6cb20003ba776e2be638cf5ec00e4e652ac9871
Andrey <andrey.onkhonov95@bk.ru>
2025-05-05 01:16:50 +0800

    Ну, самый первый — это проще всего преобразовать из числа в строку и записать перевернутую строку в отдельную переменную и потом их сравнить. Ну, собственно, нужно было еще исключить возможность попадания отрицательных чисел, они же не все будут палидромами

--- PalindromeEasyLeetCode/PalindromeEasyLeetCode.cpp
+++ PalindromeEasyLeetCode/PalindromeEasyLeetCode.cpp
@@ -1,15 +1,31 @@
 ﻿#include <iostream>
 #include <string>
+
+//Перевод из числа в строку и сравнения 
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //не потерять 
+        std::string revers;
+        std::string check_revers;
+        revers = std::to_string(x);
+
+        //Перебор символов и запись в обратном порядке
+        for (int i = revers.length() - 1; i >= 0 ; --i)
+            check_revers += revers[i];
+
+        if (check_revers == revers)
+            return true;
+        else
+            return false;
     }
 };
 
 int main()
 {
     setlocale(LC_ALL, "ru");
+
+    Solution sol;
+    std::cout << sol.isPalindrome(101);
 
     return 0;
 }
