847545f41d7f0bd71e359268ba9bc2016b7d2f74
Andrey <andrey.onkhonov95@bk.ru>
2025-05-05 01:26:10 +0800

    так чтобы не потерять #https://leetcode.com/problems/longest-palindromic-substring/description/

---
+++ len_palidromicLeetcodeMedium/len_palidromicLeetcodeMedium.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    return 0;
+}
