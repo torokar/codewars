0b443bf7af896d8194bb30f22e25eb5f910e6dc3
Andrey <andrey.onkhonov95@bk.ru>
2025-05-05 02:33:31 +0800

    Так вот такой код примерный накинули. Проблема такая, что мне как то нужно перебрать все палидромы в строке и сравнить из длину и дальше вывести палидром который самый большой

--- len_palidromicLeetcodeMedium/len_palidromicLeetcodeMedium.cpp
+++ len_palidromicLeetcodeMedium/len_palidromicLeetcodeMedium.cpp
@@ -3,14 +3,83 @@
 
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
+    std::string longestPalindrome(std::string s) 
+    {
+        std::string one_str, two_str;
+        std::string Line_redu = s;
+        int i = 0;
+        int j = 0;
+        int q = 1;
 
+        one_str = Line_redu;
+        two_str = Line_redu;
+        std::reverse(two_str.begin(), two_str.end());
+
+        if (one_str == two_str)
+        {
+            return one_str;
+        }
+
+        if (Line_redu.size() <= 2)
+        {
+            std::string ss;
+            ss += Line_redu[0];
+            return ss;
+        }
+
+        //Так ну это проверка если строка является изначально палидромом
+        //Например xabay yabax xabax 
+
+        else
+        {
+            one_str = "";
+            two_str = "";
+            while (true)
+            {
+                one_str += Line_redu[i];
+                ++i;
+                if (one_str.size() <= 1)
+                {
+                    continue;
+                }
+                while (true)
+                {
+                    two_str += one_str[j];
+                    ++j;
+                    if (j >= one_str.size())
+                    {
+                        break;
+                    }
+                }
+                std::reverse(two_str.begin(), two_str.end());
+
+                if (one_str == two_str)
+                {
+                    return one_str;
+                }
+                std::reverse(two_str.begin(), two_str.end());
+
+                //Обрезаем строку если не нашли палидром
+                //И обнуляем все значения
+                if (i >= Line_redu.size())
+                {
+                    i = 0;
+                    j = 0;
+                    Line_redu = Line_redu.substr(q, Line_redu.size());
+                    one_str = "";
+                    two_str = "";
+                }
+            }
+        }
     }
 };
 
 int main()
 {
     setlocale(LC_ALL, "ru");
+
+    Solution sol;
+    std::cout << sol.longestPalindrome("as");
 
     return 0;
 }
