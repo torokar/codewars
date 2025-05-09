f4de29ddbcb307e3afd3b02f360848fc62ebc149
Andrey <andrey.onkhonov95@bk.ru>
2025-05-09 19:58:02 +0800

    Ну на самом деле я просто взял свой старый код по фибонначи и вместо 0 и  1 вставил 1 и 2

---
+++ circle/circle.cpp
@@ -0,0 +1,20 @@
+﻿// █
+#include <iostream>
+#include <string>
+
+int nthFibo(int n)
+{
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+    return nthFibo(n - 1) + nthFibo(n - 2);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+    std::cout << nthFibo(10);
+
+	return 0;
+}
