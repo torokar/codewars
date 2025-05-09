6b4ee3a5fe9a2ba837c311ff5623ec0e6f4d3d19
Andrey <andrey.onkhonov95@bk.ru>
2025-05-09 20:15:21 +0800

    ну вот такая рекурсивная функция

--- SumofDigit/SumofDigit.cpp
+++ SumofDigit/SumofDigit.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 int digital_root(int n)
 {
@@ -7,8 +8,30 @@ int digital_root(int n)
 	{
 		return n;
 	}
+	std::string s = std::to_string(n);
+	s += '\0';
 
+	std::vector<std::string> v;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		std::string tmp;
+		tmp = s[i];
+		v.push_back(tmp);
+	}
 
+	int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		std::string revers_str = v[i];
+		int tmp = std::stoi(revers_str);
+		sum += tmp;
+	}
+
+	return digital_root(sum);
 
 }
 
@@ -16,6 +39,8 @@ int main()
 {
 	setlocale(LC_ALL, "RU");
 
+	std::cout << digital_root(19);
+	
 	return 0;
 }
 
