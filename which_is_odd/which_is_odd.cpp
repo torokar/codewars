a49ac0c1a1a9a8c2118b177e28aa336b988bb05f
Andrey <andrey.onkhonov95@bk.ru>
2025-05-12 23:38:23 +0800

    так чтобы не потерять проек ну и просто оставить ссылку А план действий такой хочу попробовать решить через многопоточность #https://www.codewars.com/kata/54da5a58ea159efa38000836

---
+++ which_is_odd/which_is_odd.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+#include <iostream>
+#include <thread>
+
+int findOdd(const std::vector<int>& numbers) 
+{
+	//your code here
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	return 0;
+}
